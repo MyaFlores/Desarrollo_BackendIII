69e177a2401503976266af2eb1aa3c18d91ca62b
Mya <mya.florescastanon@cesunbc.edu.mx>
2026-05-04 15:40:13 -0700

    Diagnostico de conocimientos

---
+++ Prueba.py
@@ -0,0 +1,13 @@
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+carrera = input("Ingrese su carrera: ")
+
+estudiante = {
+    "nombre": nombre,
+    "edad": edad,
+    "carrera": carrera
+}
+
+print("\n--- Información guardada ---")
+print(estudiante)
+
